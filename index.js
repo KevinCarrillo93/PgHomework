12748b078c9495d972a4ea7a0ccdd55b9c281737
silvina-varela <varelasilvina@gmail.com>
2022-09-12 17:46:40 -0300

    function cajaDePandora and function Silvina

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
+    if(numero % 2 === 0) return numero.toString(2);
+    else return numero.toString(16);
+}
+
+const silvina = () => {
+    return {
+        nombre: 'Silvina',
+        edad: 32,
+        nacionalidad: 'Argentina'
     }
+}
