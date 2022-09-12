70982186615867117f661bb28f162ed17e5ca14e
Fatima Insau <insaufaty@gmail.com>
2022-09-12 18:50:57 -0300

    funcion pandora y funcion fati, muchas gracias

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-function laCajaDePandora(numero){
-    // proximamente escribiremos codigo aqui
-    }
+function laCajaDePandora(numero) {
+  if (numero % 2 === 0) {
+    return numero.toString(2);
+  } else {
+    return numero.toString(16);
+  }
+}
+
+function fati2() {
+  return {
+    nombre: "fatima",
+    edad: 24,
+    nacionalidad: "argentina",
+  };
+}
