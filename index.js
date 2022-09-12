16d406d2b79adcd95f9e0f2fee70e70e3f1514a1
Yeiduin <yoelyei22@gmail.com>
2022-09-12 16:48:32 -0500

    yeiduin

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 function laCajaDePandora(numero) {
-  const num = Math.round(numero);
-  // proximamente escribiremos codigo aqui
+  const numero = Math.round(numero);
+  if (numero % 2 === 0) return numero.toString(2);
+  else return numero.toString(16);
+}
+
+function yeiduin() {
+  const me = { name: "yeiduin", age: 26, nationality: "Colombia" };
+  return me;
 }
