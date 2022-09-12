8def381901c86c2989fd4324a8f2d5666752082b
arturokaadu <arturokaadu2@gmail.com>
2022-09-12 18:06:35 -0300

    "function and name"

--- index.js
+++ index.js
@@ -1,3 +1,17 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+
+    if (numero %2 === 0){
+        const decabin = numero.toString(2)
+  return decabin
+    } else {
+        var bintodec = numero.toString(16)
+        return bintodec
+        
+    }
+    }
+
+    function Arthur(){
+        var id = { name: "arturo Kaadu", age:27, nationality: "Argentina" }
+        return id
     }
