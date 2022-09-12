5ba77c08141f88389b65a9a8e8e43e0d7feb2696
Victoria <vickyraineri04@gmail.com>
2022-09-12 18:12:33 -0300

    function cajaDePandora and function victoria

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+    if(numero % 2 !== 0) return numero.toString(16);
+    else return numero.toString(2);
+}
+
+function victoria() {
+    return {
+        name: 'victoria',
+        age: 18,
+        nationality: 'argentina'
     }
+}
