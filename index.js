4b324bff46c7e852dee6835b872e3dfd127feb94
luciorebuffo <lucio-rebuffo@hotmail.com>
2022-09-12 23:56:29 -0300

    Integrada logica en pandora y funcion lucho

--- index.js
+++ index.js
@@ -1,3 +1,19 @@
 function laCajaDePandora(numero){
     // proximamente escribiremos codigo aqui
+
+    if(numero % 2 == 0)
+    {
+        return numero.toString(2); 
     }
+    else{
+        return numero.toString(16);
+    }
+}
+
+function lucho(){
+    return {
+        name: "Lucio",
+        age: 30,
+        nationality: "Argentina",
+    }
+}
